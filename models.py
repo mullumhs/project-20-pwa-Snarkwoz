908a6e97739b1c4fd3eb6bd30035b07b75d1c461
Griffin <140124176+Snarkwoz@users.noreply.github.com>
2024-11-20 10:39:07 +1100

    set up project and created database model

--- models.py
+++ models.py
@@ -4,3 +4,13 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(50))
+    genre = db.Column(db.String(50))
+    image_url = db.Column(db.String)
+    rating = db.Column(db.Float)
+    year = db.Column(db.Integer)
+
